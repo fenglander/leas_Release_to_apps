6a9b06d9de658d0ee42f66fa1e7d268c26fd2be4
135938504+fenglander@users.noreply.github.com <135938504+fenglander@users.noreply.github.com>
2024-10-08 11:55:52 +0800

    leas_web_save_discard_but

--- leas_mes_process_reporting/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import mrp_reporting_station
-from . import mrp_bom
-from . import mrp_routing_workcenter
-from . import mrp_workorder
-from . import mrp_workcenter
-from . import mrp_workcenter_productivity
